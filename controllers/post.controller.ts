27be2de22d373aa6ad4dbd01b6f309046c033c14
faridasad <asadlifaridd@gmail.com>
2023-03-02 16:03:12 +0400

    Add createPost and getAllPosts

--- controllers/post.controller.ts
+++ controllers/post.controller.ts
@@ -1,6 +1,35 @@
 import Post from "../models/post.model";
-import { Configuration, OpenAIApi } from "openai";
+import cloudinary from "cloudinary";
+import { Request, Response } from "express";
 
-export const createPost = async (req: any, res: any) => {
-  res.send("Post created");
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+export const getAllPosts = async (req: Request, res: Response) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({ success: true, data: posts });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const createPost = async (req: Request, res: Response) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const imageUrl = await cloudinary.v2.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: imageUrl.url,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 };
