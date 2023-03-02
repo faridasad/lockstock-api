4ac6e23bca76d0d267fa149d1294b16280b97be2
faridasad <asadlifaridd@gmail.com>
2023-03-02 16:03:52 +0400

    Add upload route

--- routes/post.route.ts
+++ routes/post.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { createPost } from "../controllers/post.controller";
+import { uploadImage } from "../controllers/upload.controller";
 
 const router = Router();
+
+router.route("/dalle").post(uploadImage)
 
 router.route("/create").post(createPost);
 
