24ff6c5b8432513cd63530ea3376862b17220cc1
faridasad <asadlifaridd@gmail.com>
2023-03-02 16:02:45 +0400

    Update routes

--- server.ts
+++ server.ts
@@ -8,23 +8,12 @@ import postRouter from "./routes/post.route";
 
 const app = express();
 app.use(express.json());
-app.use("/api/post", postRouter);
+app.use("/api/v1", postRouter);
 
 connectDB(process.env.MONGO_URI!).then(() => {
   console.log("Connected to DB");
 });
 
-
-
-
-app.get("/", (req, res) => {
-  res.send("Hi mom");
-});
-
-app.post("/", async (req, res) => {
-  
-
-});
 
 app.listen({ port: parseInt(process.env.PORT!) }, () => {
   console.log(`Server is running`);
