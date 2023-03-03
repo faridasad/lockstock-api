dc1a0fc20447f682b67c2723da3c1b532928c40f
faridasad <asadlifaridd@gmail.com>
2023-03-03 22:16:51 +0400

    Use controllers

--- routes/post.route.ts
+++ routes/post.route.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller";
+import {
+  createPost,
+  getAllPosts,
+  getSinglePost,
+} from "../controllers/post.controller";
 import { uploadImage } from "../controllers/upload.controller";
 
 const router = Router();
 
-router.route("/dalle").post(uploadImage)
+router.route("/dalle").post(uploadImage);
 
 router.route("/create").post(createPost);
 
-router.route("/:id").get((req, res) => {
-  res.send("Single Post");
-});
+router.route("/").get(getAllPosts);
 
+router.route("/:id").get(getSinglePost);
 export default router;
