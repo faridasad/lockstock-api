f1ee9cb01a8b9450dfa1bf947f2b1bacd24bf50c
faridasad <asadlifaridd@gmail.com>
2023-03-03 22:15:13 +0400

    Use cors and increase payload limit

--- server.ts
+++ server.ts
@@ -1,19 +1,19 @@
 import { config } from "dotenv";
+import cors from "cors";
 config();
 import express from "express";
 import { connectDB } from "./utils/connectDB";
 
 import postRouter from "./routes/post.route";
 
-
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use("/api/v1", postRouter);
 
 connectDB(process.env.MONGO_URI!).then(() => {
   console.log("Connected to DB");
 });
-
 
 app.listen({ port: parseInt(process.env.PORT!) }, () => {
   console.log(`Server is running`);
