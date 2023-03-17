5708c134a2c9ed93bf5bb976a74834aefa42cc67
faridasad <asadlifaridd@gmail.com>
2023-03-17 23:55:10 +0400

    Send page data

--- controllers/post.controller.ts
+++ controllers/post.controller.ts
@@ -10,8 +10,25 @@ cloudinary.v2.config({
 
 export const getAllPosts = async (req: Request, res: Response) => {
   try {
-    const posts = await Post.find();
-    res.status(200).json({ success: true, data: posts });
+    const page = parseInt(req.query.page as string) || 1;
+    const perPage = parseInt(req.query.perPage as string) || 10;
+    const skip = (page - 1) * perPage;
+
+    const posts = await Post.find().skip(skip).limit(perPage);
+    const totalPosts = await Post.countDocuments();
+
+    const totalPages = Math.ceil(totalPosts / perPage);
+
+    const nextPage = page + 1 > totalPages ? null : page + 1;
+
+    res.status(200).json({
+      success: true,
+      data: posts,
+      currentPage: page,
+      totalPages,
+      totalPosts,
+      nextPage,
+    });
   } catch (err: any) {
     res.status(500).json({ success: false, message: err.message });
   }
